76b281cb8023762a538cb41db9ee52aa19c28924
ib <ib@192.168.162.12>
2017-10-05 14:11:46 +0200

    mon site - twig

--- front_controleur.php
+++ front_controleur.php
@@ -1,15 +1,26 @@
 <?php
-include './lib/lib_db.php';
+include_once './lib/lib_db.php';
+require_once './vendor/autoload.php' ;
+$loader = new Twig_Loader_Filesystem("TWIG");
+$twig = new Twig_Environment($loader);
+
+
+//include './lib/lib_db.php';
 //static $films = ["Dracula", "Kung fu Panda", "dora l'exploratrice"];
-$films= listerFilms();
- session_start();
+//$films= listerFilms();
+
+
+
+session_start();
 @$action = $_REQUEST ["action"];
 $pagecontenu = "";
+
 switch ($action) {
     case "liste_films" :
         //récupère les films : $fims
         //Renvoie à la vue
-        $pagecontenu = './liste_films.php';
+        //$pagecontenu = './liste_films.php';
+        echo $twig->load("liste_films.html.twig")->render(["films"=> listerfilms()]);
         break;
     case "ajoute_film" :
         $pagecontenu = './ajoute_film.php';
@@ -31,18 +42,18 @@ switch ($action) {
     case "supprime_serie":
         break;
     case "login":
-        $pagecontenu = './login.php';
+        //$pagecontenu = './login.php';
+        echo $twig->load("login.html.twig") ->render(["films"=> listerfilms()] );
         break;
     case "login_post":
         @$login = $_REQUEST ["login"];
         @$mdp = $_REQUEST ["mdp"];
         if ($login == "admin" && $mdp == "123") {
-           
             @$_SESSION["utilConnecte"] = $login;
             header("Location: front_controleur.php?action=liste_films");
             exit;
         } else {
-            $pagecontenu = './login.php';
+            echo $twig->load("login.html.twig")->render(["films"=> listerfilms(), "utilconnecte"=>$login]);
             break;
         }
         break;
@@ -55,10 +66,9 @@ switch ($action) {
         break;
 }
 
-echo $pagecontenu;
 //afficher l page par composition
 
-include './_HEADER.php';
-include './_MENU.php';
-include $pagecontenu;
-include './_FOOTER.php';
+//include './_HEADER.php';
+//include './_MENU.php';
+//include $pagecontenu;
+//include './_FOOTER.php';
---
+++ twig_test.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once './lib/lib_db.php';
+require_once './vendor/autoload.php' ;
+$loader = new Twig_Loader_Filesystem("TWIG");
+$twig = new Twig_Environment($loader);
+
+echo $twig->load("liste_films.html.twig")->render(["films"=> listerfilms()]);
